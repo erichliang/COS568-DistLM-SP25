6a6d382061440ca3c7d0a1f9770ad95a7a6ed26e
Erich Liang <erichliang08@gmail.com>
2025-03-28 12:41:36 -0400

    test 2a

--- task2a/test.py
+++ task2a/test.py
@@ -1,16 +1,13 @@
 import torch
 import torch.distributed as dist
+import argparse
 import os
 
-def init_distributed_mode(backend="gloo"):
-    # Set up master IP and port
-    master_ip = "10.10.1.24"  # The IP of the master node
-    master_port = "12355"  # The port to use for communication
-
+def init_distributed_mode(master_ip, master_port, local_rank, world_size, backend="gloo"):
     # Get rank and world size from environment variables
-    rank = int(os.environ["RANK"])
+    rank = int(local_rank)
     print(rank)
-    world_size = int(os.environ["WORLD_SIZE"])
+    world_size = int(world_size)
     print(world_size)
 
     # Initialize the process group for distributed training
@@ -25,5 +22,16 @@ def init_distributed_mode(backend="gloo"):
 
 # This is just for testing purposes, you would need to run this on all nodes
 if __name__ == "__main__":
-    init_distributed_mode()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--master_ip", type=str, default="10.10.1.24")
+    parser.add_argument("--master_port", type=str, default="12355")
+    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank. If single-node training, local_rank defaults to -1.")
+    args = parser.parse_args()
+    
+    master_ip = args.master_ip
+    master_port = args.master_port
+    local_rank = args.local_rank
+    world_size = 4
+    
+    init_distributed_mode(master_ip, master_port, local_rank, world_size)
 
